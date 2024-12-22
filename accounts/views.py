3904ac55cdf25594948df632d007d217b71bc779
kristian <krestrepoo@eafit.edu.co>
2024-12-21 20:09:27 -0500

    feat: add customised user creation form

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from .forms import CustomUserCreationForm
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html', {'template_data': template_data})
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home.index')
