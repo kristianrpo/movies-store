39043f4dc8c174650396ce22d9b574e62425c379
kristian <krestrepoo@eafit.edu.co>
2024-12-23 12:35:34 -0500

    feat: add requirements.txt file

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p_j)qm5$*1#f8v8b5xe!+xfv%3hab782!63cp%)^cmmrd^u_$g"
+SECRET_KEY = config('SECRET_KEY', default = "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default = False, cast=bool)
 
 ALLOWED_HOSTS = []
 
